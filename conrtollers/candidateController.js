21c5c53061ba213fcf1e5aca0f503d3da7d95acb
MadhuriYadav2521 <madhuri.y.0932@gmail.com>
2023-08-10 10:35:52 +0530

    updated

--- conrtollers/candidateController.js
+++ conrtollers/candidateController.js
@@ -3,3 +3,19 @@ import Candidates from "../modals/candidateModal.js"
 export const renderAddCandidate = (req,res)=>{
     return res.render('addCandidate')
 }
+
+export const addCandidate = async (req,res) =>{
+    try{
+        const{candidateName} = req.body
+        if(!candidateName) return res.render('addCandidate',{message: "Candidate name is manadatory!"})
+        const existingCandidate = await Candidates.findOne({candidateName}).exec();
+        if(existingCandidate) return res.render('addCandidate',{message:"Candidate already exist!"})
+        const candidate = new Candidates({
+            candidateName
+        })
+        await candidate.save();
+        return res.render('addCandidate', {success: "Candidate added sussessfully!"})
+    }catch(err){
+        return res.send(err)
+    }
+}
--- routes/candidateRoute.js
+++ routes/candidateRoute.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { renderAddCandidate } from "../conrtollers/candidateController.js";
+import { addCandidate, renderAddCandidate } from "../conrtollers/candidateController.js";
 import { isAdminLogin, isLogin } from "../middlewares/auth.js";
 
 const candidateRouter = express.Router()
 
 candidateRouter.get('/addCandidate',isLogin,isAdminLogin,renderAddCandidate)
+candidateRouter.post('/addCandidate',addCandidate)
 
 
 
